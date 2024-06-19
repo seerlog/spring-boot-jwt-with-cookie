881065459db553adde940c4d31cd89ef42a1d8f3
이수민 <smlee@twolinecode.com>
2024-06-19 11:58:54 +0900

    first commit

---
+++ src/main/java/org/example/springbootjwtwithcookie/SpringBootJwtWithCookieApplication.java
@@ -0,0 +1,13 @@
+package org.example.springbootjwtwithcookie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtWithCookieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJwtWithCookieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springbootjwtwithcookie/SpringBootJwtWithCookieApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springbootjwtwithcookie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtWithCookieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
